6eaee908fe5d504e97a401c630fb588d6cd212ef
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-10-21 23:19:07 -0400

    Done with ProductContext

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,9 +22,11 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			<ProductContext.Provider value = {products, addItem}>
+				<Route exact path="/">
+					<Products />
+				</Route>
+			</ProductContext.Provider>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
